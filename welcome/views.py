28c0b04db0ed65ef03fbcbb15ae7d0627cf65595
mdzafar <mdzafar@tradeindia.com>
2019-09-17 17:36:31 +0530

    Added CSS and some content in index html page

--- welcome/views.py
+++ welcome/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 
@@ -6,8 +6,12 @@ from django.http import HttpResponse
 
 def index(request):
     context = {}
-
     return render(request, 'index.html', context)
 
 def demo(request):
     return HttpResponse('demo page')
+
+def contact_us(request):
+    print("test")
+    return HttpResponse('demo page')
+    # return redirect('/')
